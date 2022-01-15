5dd6b8403fac36f55cf8241499fed042034679f9
Avinesh Culloo <avineshculloo@gmail.com>
2022-01-15 18:06:20 +0400

    Add carousel

--- Code/Website/common/common.php
+++ Code/Website/common/common.php
@@ -32,7 +32,12 @@ function generateHeader($title, $directoryname){
             
             echo '<!-- External '. $directoryname . ' CSS -->';
             echo '<link href="./'. $directoryname .'/css/styles.css" type="text/css" rel="stylesheet">';
-
+            
+            echo '<!-- Bootstrap JS -->';
+            echo '<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>';
+            echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>';
+            echo '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>';
+            
         } else {
             // other directory locations
             echo '<!-- Tab icon-->';
--- Code/Website/index.php
+++ Code/Website/index.php
@@ -10,7 +10,70 @@
     generateHeader($pageName, $folderName);
     generateNavBar($pageName);
 ?>
-
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <div class="carousel-content">
+                <div class="carousel-image">
+                    <img src="./common/img/watercolor_landscape_mauritius.jpg" alt="landscape mauritius" width= "100%" 
+                    height="500px">
+                </div>
+                <div class="carousel-side-caption">
+                    <h5>Mauritius Landscape</h5>
+                    <p>Mauritius was made first, and then heaven; and heaven
+                        was copied after Mauritius. 
+                    </p>
+                    <p id="author">
+                    Mark Twain
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="carousel-content">
+                <div class="carousel-image">
+                    <img src="./common/img/watercolor_LeMorne.jfif" alt="le morne" width= "100%" 
+                        height="500px">
+                </div>
+                <div class="carousel-side-caption">
+                    <h5>Le Morne</h5>
+                    <p>Le Morne Mountain represents marronage and its impact in Mauritius. 
+                        It is a symbol of the slaves’ fight for freedom, their suffering, 
+                        and their sacrifice. 
+                    </p>
+                    <p id="author">
+                        UNESCO
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="carousel-content">
+                <div class="carousel-image">
+                    <img src="./common/img/sega.jpg" alt="sega"width= "100%" 
+                    height="500px">
+                </div>
+                <div class="carousel-side-caption">
+                    <h5>Le Sega Mauricien</h5>
+                    <p>The Sega is a cry from the soul trying to transcend the miseries and heartaches of life,
+                         while at the same time expressing the universal human desire for joy and happiness.
+                    </p>
+                    <p id="author">
+                        Dinesh Sobhee
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 <?php
     // php function to generate the footer
     generateFooter($pageName);
