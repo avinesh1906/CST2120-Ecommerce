6c638935819969838cc18f2ce8963fe3ae3d1dbc
Avinesh Culloo <avineshculloo@gmail.com>
2022-01-15 16:20:47 +0400

    Add generateheader + generateFooter in index.php

---
+++ Code/Website/index.php
@@ -0,0 +1,17 @@
+<?php
+    // php variables
+    $pageName = 'Home';
+    $folderName = 'home';
+
+    // include the common.php which contains the php functions
+    include('./common/common.php');
+
+    // call the php functions 
+    generateHeader($pageName, $folderName);
+    generateNavBar($pageName);
+?>
+
+<?php
+    // php function to generate the footer
+    generateFooter($pageName);
+?>0
