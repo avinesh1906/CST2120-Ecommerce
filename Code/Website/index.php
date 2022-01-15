05f7e64e95e6e118a33279aad9a6c37619c80591
Avinesh Culloo <avineshculloo@gmail.com>
2022-01-15 23:02:58 +0400

    Added top product + images

--- Code/Website/index.php
+++ Code/Website/index.php
@@ -74,6 +74,172 @@
     <span class="sr-only">Next</span>
   </a>
 </div>
+
+<div class="top-product">
+    <div class="title">
+        <h4> Top Product </h4>
+    </div>
+    <div class="options">
+        <ul class="options-item">
+            <li> FEATURED </li>
+            <li> LATEST </li>
+            <li> BESTSELLER </li>
+        </ul>
+    </div>
+
+    <div class="featured">
+        <div class="card-deck row">
+            <div class="card-scroll">
+                <img class="card-img-top" src="./common/img/marilyn.jpeg" alt="marilyn" height="450px" width="95%">
+                <div class="card-body">
+                    <h5 class="card-title">Marilyn Monroe</h5>
+                    <p class="card-text">If I'd observed all the rules, I'd never have got anywhere</p>
+                    <div class="card-details">
+                        <p>Rs 1500 </p>
+                        <button style="font-size:20px"> <i class="fa fa-shopping-basket"></i> Basket </button>
+                    </div>
+                </div>
+            </div>
+            <div class="card-scroll">
+                <img class="card-img-top" src="./common/img/lion.jpeg" alt="lion" height="450px" width="95%">
+                <div class="card-body">
+                    <h5 class="card-title"> Le Désir d'imagination</h5>
+                    <p class="card-text">L’imagination fait du désir une passion heureuse.</p>
+                    <div class="card-details">
+                        <p>Rs 800 </p>
+                        <button style="font-size:20px"> <i class="fa fa-shopping-basket"></i> Basket </button>
+                    </div>
+                </div>
+            </div>
+            <div class="card-scroll">
+                <img class="card-img-top" src="./common/img/women_face.jpeg" alt="women face" height="450px" width="95%">
+                <div class="card-body">
+                    <h5 class="card-title">La beauté de faiblesse</h5>
+                    <p class="card-text">La beauté sans grâce  est un printemps sans verdure.
+                    </p>
+                    <div class="card-details">
+                        <p>Rs 1250 </p>
+                        <button style="font-size:20px"> <i class="fa fa-shopping-basket"></i> Basket </button>
+                    </div>    
+                </div>
+            </div>
+            <div class=" card-scroll">
+                <img class="card-img-top" src="./common/img/chinese.jpeg" alt="Chinese" height="450px" width="95%">
+                <div class="card-body">
+                    <h5 class="card-title">Shameless spring</h5>
+                    <p class="card-text">The first blooms of spring always make my heart sing</p>
+                    <div class="card-details">
+                        <p>Rs 850 </p>
+                        <button style="font-size:20px"> <i class="fa fa-shopping-basket"></i> Basket </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="latest">
+        <div class="card-deck row">
+            <div class="card-scroll">
+                <img class="card-img-top" src="./common/img/cat.jpeg" alt="cat" height="450px" width="95%">
+                <div class="card-body">
+                    <h5 class="card-title">Marilyn Monroe</h5>
+                    <p class="card-text">If I'd observed all the rules, I'd never have got anywhere</p>
+                    <div class="card-details">
+                        <p>Rs 1500 </p>
+                        <button style="font-size:20px"> <i class="fa fa-shopping-basket"></i> Basket </button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-scroll">
+                <img class="card-img-top" src="./common/img/women.jpeg" alt="women" height="450px" width="95%">
+                <div class="card-body">
+                    <h5 class="card-title">La beauté de faiblesse</h5>
+                    <p class="card-text">La beauté sans grâce  est un printemps sans verdure.
+                    </p>
+                    <div class="card-details">
+                        <p>Rs 1250 </p>
+                        <button style="font-size:20px"> <i class="fa fa-shopping-basket"></i> Basket </button>
+                    </div>    
+                </div>
+            </div>
+            <div class=" card-scroll">
+                <img class="card-img-top" src="./common/img/sunset.jpeg" alt="Chinese" height="450px" width="95%">
+                <div class="card-body">
+                    <h5 class="card-title">Shameless spring</h5>
+                    <p class="card-text">The first blooms of spring always make my heart sing</p>
+                    <div class="card-details">
+                        <p>Rs 850 </p>
+                        <button style="font-size:20px"> <i class="fa fa-shopping-basket"></i> Basket </button>
+                    </div>
+                </div>
+            </div>
+            <div class=" card-scroll">
+                <img class="card-img-top" src="./common/img/lacote.png" alt="lacote" height="450px" width="95%">
+                <div class="card-body">
+                    <h5 class="card-title">Shameless spring</h5>
+                    <p class="card-text">The first blooms of spring always make my heart sing</p>
+                    <div class="card-details">
+                        <p>Rs 850 </p>
+                        <button style="font-size:20px"> <i class="fa fa-shopping-basket"></i> Basket </button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <div class="bestseller">
+        <div class="card-deck row">
+            <div class="card-scroll">
+                <img class="card-img-top" src="./common/img/wolves.jpeg" alt="wolf" height="450px" width="95%">
+                <div class="card-body">
+                    <h5 class="card-title"> Le Désir d'imagination</h5>
+                    <p class="card-text">L’imagination fait du désir une passion heureuse.</p>
+                    <div class="card-details">
+                        <p>Rs 800 </p>
+                        <button style="font-size:20px"> <i class="fa fa-shopping-basket"></i> Basket </button>
+                    </div>
+                </div>
+            </div>
+            <div class="card-scroll">
+                <img class="card-img-top" src="./common/img/watercolor_landscape_mauritius.jpg" alt="mauritius landscape" height="450px" width="95%">
+                <div class="card-body">
+                    <h5 class="card-title">La beauté de faiblesse</h5>
+                    <p class="card-text">La beauté sans grâce  est un printemps sans verdure.
+                    </p>
+                    <div class="card-details">
+                        <p>Rs 1250 </p>
+                        <button style="font-size:20px"> <i class="fa fa-shopping-basket"></i> Basket </button>
+                    </div>    
+                </div>
+            </div>
+            <div class=" card-scroll">
+                <img class="card-img-top" src="./common/img/sunset.jpeg" alt="Chinese" height="450px" width="95%">
+                <div class="card-body">
+                    <h5 class="card-title">Shameless spring</h5>
+                    <p class="card-text">The first blooms of spring always make my heart sing</p>
+                    <div class="card-details">
+                        <p>Rs 850 </p>
+                        <button style="font-size:20px"> <i class="fa fa-shopping-basket"></i> Basket </button>
+                    </div>
+                </div>
+            </div>
+            <div class=" card-scroll">
+                <img class="card-img-top" src="./common/img/lacote.png" alt="lacote" height="450px" width="95%">
+                <div class="card-body">
+                    <h5 class="card-title">Shameless spring</h5>
+                    <p class="card-text">The first blooms of spring always make my heart sing</p>
+                    <div class="card-details">
+                        <p>Rs 850 </p>
+                        <button style="font-size:20px"> <i class="fa fa-shopping-basket"></i> Basket </button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
 <?php
     // php function to generate the footer
     generateFooter($pageName);
