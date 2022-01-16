250ece5a28a6a9b2768583a8f0a1b589cbeeb706
Avinesh Culloo <avineshculloo@gmail.com>
2022-01-16 14:03:25 +0400

    Create the signin window + its css

---
+++ Code/Website/signin/signin.php
@@ -0,0 +1,61 @@
+<?php
+    // php variables
+    $pageName = 'Sign In';
+    $folderName = 'sigin';
+
+    // include the common.php which contains the php functions
+    include('../common/common.php');
+
+    // call the php functions 
+    generateHeader($pageName, $folderName);
+    generateNavBar($pageName);
+?>
+<div class="signIn-title">
+    <h1>Already Registered?</h1>
+</div>
+<div class="signIn-body">
+    <div class="register">
+        <div class="title">
+            New Customer
+        </div>
+        <div class="body">
+            By creating an account you will be able to shop 
+            faster, be up to date on an order's status, and 
+            keep track of the orders you have previously made.
+        </div>
+        <!-- Register button -->
+        <div class="registerBtn">
+            <button>Create Account</button>   
+        </div>
+    </div>
+    <div class="login">
+        <div class="title">
+            Login
+        </div>
+        <div class="body">
+            If you have an account, please log in.
+            <!-- Login form -->
+            <section name="login_form">
+                <div class="login_form ">
+                    <div class="username">
+                        <input autocomplete="off" type="text" class="form-control" id="Username" onkeyup="usernameValidation()" placeholder="username">
+                        <span id="usr_details"></span>
+                    </div>
+                    <div class="password">
+                        <input  autocomplete="off" type="password" class="form-control" id="Password" onkeyup="passwordValidation()" placeholder="password" >
+                        <span id="pwd_details"></span>
+                    </div>
+                </div>
+                <!-- Login submit button -->
+                <div class="login_btn">
+                    <button>Sign In </button>   
+                </div>
+            <section>
+        </div>
+    </div>
+</div>
+<?php
+    // php function to generate the footer
+    generateJavaScript($pageName);
+    generateFooter($pageName);
+?>
