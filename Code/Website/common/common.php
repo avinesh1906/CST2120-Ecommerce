a9df159a371419f0e270299f5f5c74496bf66f2d
Avinesh Culloo <avineshculloo@gmail.com>
2022-01-17 22:49:10 +0400

    Create the content for oil painting + its css

--- Code/Website/common/common.php
+++ Code/Website/common/common.php
@@ -15,8 +15,7 @@ function generateHeader($title, $directoryname){
         echo '<title>Ti Moris: ' . $title . '</title>';
 
         echo '<!-- Bootstrap CSS -->';
-        echo '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">';
-        
+        echo '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">';
         echo '<!-- icon CSS -->';
         echo '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">';
         
@@ -34,10 +33,12 @@ function generateHeader($title, $directoryname){
             echo '<link href="./'. $directoryname .'/css/styles.css" type="text/css" rel="stylesheet">';
             
             echo '<!-- Bootstrap JS -->';
-            echo '<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>';
-            echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>';
-            echo '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>';
-            
+                    echo'<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>';
+        echo '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>';
+        echo '<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>';
+        echo '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>';
+        
+           
         } else {
             // other directory locations
             echo '<!-- Tab icon-->';
@@ -48,6 +49,9 @@ function generateHeader($title, $directoryname){
             
             echo '<!-- External '. $directoryname . ' CSS -->';
             echo '<link href="./css/styles.css" type="text/css" rel="stylesheet">';
+            echo '<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>        ';
+    
         }
 
         echo '</head>';
@@ -231,9 +235,12 @@ function generateFooter($pagename){
 }
 
 function generateJavaScript($title){
-    if ($title == "Home") {
+    if ($title == "Home" ) {
         echo '<script type="module" src="./home/js/home.js">';
+    } else if ($title ==  "Oil"){
+        echo '<script type="module" src="./js/oil.js">';
     }
+
     echo '</script>';
     echo '</body>';
     echo '</html>';
--- Code/Website/oil/oil.php
+++ Code/Website/oil/oil.php
@@ -10,6 +10,203 @@
     generateHeader($pageName, $folderName);
     generateNavBar($pageName);
 ?>
+<!-- Title -->
+<div class="product-title">
+    Oil Painting
+    <!-- Title bottom border line -->
+    <div id="borderBot"></div>
+</div>
+<!-- Description -->
+<div class="product-description">
+    <!-- Image -->
+    <div class="image">
+        <img src="../common/img/dodo_oil.jpg" alt="dodo oil" 
+            height="250px">
+    </div>
+    <!-- Body Content -->
+    <div class="body-description">
+    Oil painting is a type of painting produced using oil-based paints. 
+    It involves using pigments that use a medium of drying oil as the binder and painting with them on a canvas.
+    <br>
+    Oil paints are one of the most well-respected and popular types of paints and have long been a favored medium 
+    of fine artists. Oil paints produce excellent tones and colors which differentiate oil paints from other painting 
+    mediums and materials. Oil paints can also create both satisfactory linear treatment and crisp effects. 
+    </div>
+</div>
+<!-- Shopping container -->
+<div class="shopping">
+    <!-- Filter container -->
+    <div class="filter">
+        <!-- Availability filter -->
+        <div class="availability">
+            <!-- Title -->
+            <div class="availability-title">
+                <a> AVAILABILITY </a>
+                <div class="lineBreak"></div>
+                <div id="details"> Click on box to filter availability </div>
+            </div>
+            <!-- Checkbox -->
+            <div class="availability-list">  
+                <input type="checkbox" id="instock" name="instock" value="InStock">
+                <label for="instock"> In Stock</label> 
+                <br>
+                <input type="checkbox" id="outofstock" name="outofstock" value="OutOfStock">
+                <label for="outofstock"> Out of Stock</label>
+            </div>
+        </div>
+        <!-- Price filter -->
+        <div class="price">
+            <!-- Title -->
+            <div class="price-title">
+                <a> PRICE </a>
+                <div class="lineBreak"></div>
+                <div id="details">  Use slider or enter min and max price  </div>
+            </div>
+            <!-- Price range -->
+            <div class="price-range">  
+                <!-- Price Input box -->
+                <div class="price-input">
+                    <!-- Min range -->
+                    <div class="range">
+                        <span> Min </span>
+                        <input type="number" class="minInput" value="500">
+                    </div>
+                    <!-- - between input box -->
+                    <div class="separator">-</div>
+                    <!-- Max range -->
+                    <div class="range">
+                        <span> Max </span>
+                        <input type="number" class="maxInput" value="1500">
+                    </div>
+                </div>
+                <!-- Slider  -->
+                <div class="slider">
+                    <div class="progress"></div>
+                </div>
+                <!-- Range Input -->
+                <div class="range-input">
+                    <input type="range" class="range-min" min="0" max="2000" value="500" step="100">
+                    <input type="range" class="range-max" min="0" max="2000" value="1500" step="100">
+                </div>
+            </div>
+        </div>
+        <!-- Size filter -->
+        <div class="size">
+            <!-- Title -->
+            <div class="size-title">
+                <a> SIZE </a>
+                <div class="lineBreak"></div>
+                <div id="details">  Choose your painting size  </div>
+            </div>
+            <!-- Size checklist -->
+            <div class="size-checklist">  
+                <input type="checkbox" id="A2" name="A2" value="A2">
+                <label for="A2"> 420 x 594 mm</label>
+                <br>
+                <input type="checkbox" id="A3" name="A3" value="A3">
+                <label for="A3">297 x 420 mm</label>
+                <br>
+                <input type="checkbox" id="A4" name="A4" value="A4">
+                <label for="A4">210 x 297 mm</label>
+            </div>
+        </div>
+    </div>
+    <!-- All product list -->
+    <div class="product-list">
+        <!-- Header -->
+        <div class="header">
+            <!-- Default dropstart button -->
+            <div class="btn-group dropstart">
+                <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                    Sort By
+                </button>
+                <ul class="dropdown-menu">
+                    <!-- Dropdown menu links -->
+                    <li><button class="dropdown-item" type="button">Featured</button></li>
+                    <li><button class="dropdown-item" type="button">Best Selling</button></li>
+                    <li><button class="dropdown-item" type="button">Price, Low to High</button></li>
+                    <li><button class="dropdown-item" type="button">Price, High to Low</button></li>
+                    <li><button class="dropdown-item" type="button">Alphabetically, A to Z</button></li>
+                    <li><button class="dropdown-item" type="button">Alphabetically, Z to A</button></li>
+                </ul>
+            </div>
+        </div>
+        <div class="content">
+            <div class="row row-cols- row-cols-md-3 g-4">
+                <div class="col">
+                    <div class="card">
+                        <!-- image -->
+                        <img class="card-img-top" src="./img/horse.jpg" alt="horse" height="150px" width="80%">
+                        <!-- content -->
+                        <div class="card-body">
+                            <h5 class="card-title">Mauritius Horses</h5>
+                            <div class="card-details">
+                                <p>Rs 350 </p>
+                                <button style="font-size:20px"> <i class="fa fa-shopping-basket"></i> Basket </button>
+                            </div>    
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <!-- image -->
+                        <img class="card-img-top" src="./img/lion.jfif" alt="lion" height="150px" width="80%">
+                        <!-- content -->
+                        <div class="card-body">
+                            <h5 class="card-title">Lion Mountain</h5>
+                            <div class="card-details">
+                                <p>Rs 750 </p>
+                                <button style="font-size:20px"> <i class="fa fa-shopping-basket"></i> Basket </button>
+                            </div>    
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <!-- image -->
+                        <img class="card-img-top" src="./img/boat.jpg" alt="boat" height="150px" width="80%">
+                        <!-- content -->
+                        <div class="card-body">
+                            <h5 class="card-title">Sunset</h5>
+                            <div class="card-details">
+                                <p>Rs 1750 </p>
+                                <button style="font-size:20px"> <i class="fa fa-shopping-basket"></i> Basket </button>
+                            </div>    
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <!-- image -->
+                        <img class="card-img-top" src="./img/mbg.jfif" alt="mbg" height="150px" width="80%">
+                        <!-- content -->
+                        <div class="card-body">
+                            <h5 class="card-title">Mahebourg Waterfront</h5>
+                            <div class="card-details">
+                                <p>Rs 1250 </p>
+                                <button style="font-size:20px"> <i class="fa fa-shopping-basket"></i> Basket </button>
+                            </div>    
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <!-- image -->
+                        <img class="card-img-top" src="./img/fishing.jfif" alt="fishing" height="150px" width="80%">
+                        <!-- content -->
+                        <div class="card-body">
+                            <h5 class="card-title">Out for fishing</h5>
+                            <div class="card-details">
+                                <p>Rs 1000 </p>
+                                <button style="font-size:20px"> <i class="fa fa-shopping-basket"></i> Basket </button>
+                            </div>    
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php
     // php function to generate the footer
