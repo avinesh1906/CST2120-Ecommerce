69620bc05723502336074d0f0fb5e55ca18b4a68
Smreeti1230 <SJ983@live.mdx.ac.uk.com>
2022-01-20 14:51:46 +0400

    all

--- Code/CMS/PHP/common.php
+++ Code/CMS/PHP/common.php
@@ -1,6 +1,5 @@
 <?php
     function navbar() {
-        echo '<body>';
         echo '<nav>';
         echo '<div class="menu-icon">';
         echo '<span class="fas fa-bars"></span>';
@@ -26,61 +25,7 @@
         echo '<span class="fas fa-times"></span>';
         echo '</div>';
         echo '</nav>';
-
     }
-
-    function footer() {
-        echo '<footer class="footer1">';
-        echo '<div class="footer-left">';
-        echo '<h3>Ti<span>Moris</span></h3>';
-        echo '<p class="footer-links">';       
-        echo '<a href="#">Home</a>';
-        echo '.';
-        echo '<a href="#">Login</a> ';
-        echo '.';
-        echo '<a href="#">About</a>';
-        echo '.';
-        echo '<a href="#">Privacy Policy</a>';
-        echo '.';
-        echo '<a href="#">Faq</a>';
-        echo '.';
-        echo '<a href="#">Contact</a>';
-        echo '</p>';
-        echo '<p class="footer-company-name">Ti Moris &copy; 2022</p>';
-        echo '</div>';
-        echo '<div class="footer-center">';
-        echo '<div>';
-        echo '<i class="fa fa-map-marker"></i>';
-        echo '<p><span>Avenue des Palmiers </span> Curepipe,Mauritius</p>';
-        echo '</div>';
-        echo '<div>';
-        echo '<i class="fa fa-phone"></i>';
-        echo '<p>+230 59484598</p>';
-        echo '</div>';
-        echo '<div>';
-        echo '<i class="fa fa-envelope"></i>';
-        echo '<p><a href="SJ983@live.mdx.ac.uk">SJ983@live.mdx.ac.uk</a></p>';
-        echo '</div>';
-        echo '<p><a href="SJ983@live.mdx.ac.uk">SJ983@live.mdx.ac.uk</a></p>';
-        echo '</div>';
-        echo '</div>';
-        echo '<div class="footer-right">';
-        echo '<p class="footer-company-about">';
-        echo '<span>About the company</span>';
-        echo 'Ecommerce website selling local paintings</p>';
-        echo '<div class="footer-icons">';
-        echo '<a href="#"><i class="fa fa-facebook"></i></a>';
-        echo '<a href="#"><i class="fa fa-twitter"></i></a>';
-        echo '<a href="#"><i class="fa fa-linkedin"></i></a>';
-        echo '<a href="#"><i class="fa fa-github"></i></a>';
-        echo '</div> ';
-        echo '</footer>';
-        echo '</body>';
-
-
-    }
-
-
 
 ?>
  
--- Code/CMS/index.php
+++ Code/CMS/index.php
@@ -6,12 +6,11 @@
     <title>Main Page </title>
     <?php include ("PHP/common.php");
     navbar();
-    footer();
     ?>
     <link rel="stylesheet"type="text/css" href="../CSS/index.css">
     <link href='https://fonts.googleapis.com/css?family=Nunito:400,300' rel='stylesheet' type='text/css'>
 </head>
-
+<body>
 
      <div class="sidemenu">
         <div class="logo-detailing">
@@ -265,4 +264,59 @@
     }
      </script>
 
+<footer class="footer1">
+  
+  <div class="footer-left">
+  <h3>Ti<span>Moris</span></h3>
+  <p class="footer-links">
+  <a href="#">Home</a>
+  .
+  <a href="#">Login</a>
+  ·
+  <a href="#">About</a>
+  ·
+  <a href="#">Privacy Policy</a>
+  ·
+  <a href="#">Faq</a>
+  ·
+  <a href="#">Contact</a>
+  </p>
+  
+  <p class="footer-company-name">Ti Moris &copy; 2022</p>
+  </div>
+  
+  <div class="footer-center">
+  <div>
+  <i class="fa fa-map-marker"></i>
+  <p><span>Avenue des Palmiers </span> Curepipe,Mauritius</p>
+  </div>
+  
+  <div>
+  <i class="fa fa-phone"></i>
+  <p>+230 59484598</p>
+  </div>
+  
+  <div>
+  <i class="fa fa-envelope"></i>
+  <p><a href="SJ983@live.mdx.ac.uk">SJ983@live.mdx.ac.uk</a></p>
+  </div>
+  
+  </div>
+  
+  <div class="footer-right">
+  
+  <p class="footer-company-about">
+  <span>About the company</span>
+  Ecommerce website selling local paintings&amp; 
+  </p>
+  <div class="footer-icons">
+  <a href="#"><i class="fa fa-facebook"></i></a>
+  <a href="#"><i class="fa fa-twitter"></i></a>
+  <a href="#"><i class="fa fa-linkedin"></i></a>
+  <a href="#"><i class="fa fa-github"></i></a>
+  
+  </div>  
+  </footer>
+  </body>
+</html>
   
