1e2fdfd644b6103b1e489d6f9631a1186674cca9
Smreeti1230 <SJ983@live.mdx.ac.uk.com>
2022-01-20 13:02:56 +0400

    new changes

--- Code/CMS/PHP/index.php
+++ Code/CMS/PHP/index.php
@@ -3,16 +3,38 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign Up Form</title>
-    <?php include ("PHP/common.php");
-    navbar();
-    footer();
-
-    ?>
+    <title>Main Page </title>
     <link rel="stylesheet"type="text/css" href="../CSS/index.css">
     <link href='https://fonts.googleapis.com/css?family=Nunito:400,300' rel='stylesheet' type='text/css'>
 </head>
 <body>
+
+   
+    <nav>
+        <div class="menu-icon">
+           <span class="fas fa-bars"></span>
+        </div>
+        <div class="logo">
+        <img src="../IMAGES/Ti Moris.png"alt="Ti Moris"width="100" height="80" >
+           Ti Moris
+        </div>
+        <div class="nav-items">
+           <li><a href="#">Home</a></li>
+           <li><a href="#">Sign up</a></li>
+           <li><a href="#">Add Products</a></li>
+           <li><a href="#">View Products</a></li>
+           <li><a href="#">Delete Products</a></li>
+           <li><a href="#">Edit Products</a></li>
+           <li><a href="#">Customer Orders</a></li>
+           
+        </div>
+        <div class="search-icon">
+           <span class="fas fa-search"></span>
+        </div>
+        <div class="cancel-icon">
+           <span class="fas fa-times"></span>
+        </div>
+     </nav>
 
      <div class="sidemenu">
         <div class="logo-detailing">
@@ -212,29 +234,41 @@
             <div class="top-sales box">
               <div class="title">Best Selling Products</div>
               <ul class="top-sales-details">
-              <li>
+                <li>
                 <a href="#">
-                  <span class="product">Zenfan la Cote</span>
+                  <span class="product">Landscape of Mahebourg</span>
                 </a>
-                <span class="price">Rs 850</span>
+                <span class="price">Rs 1500</span>
               </li>
               <li>
                 <a href="#">
-                  <span class="product">Soleil p alle dormi</span>
+                  <span class="product">Port Louis the port </span>
                 </a>
-                <span class="price">Rs 850</span>
+                <span class="price">Rs 475</span>
               </li>
               <li>
                 <a href="#">
-                  <span class="product">Feel zafr la</span>
+                  <span class="product">Le Morne Brabant Landscape</span>
                 </a>
-                <span class="price">Rs 1250</span>
+                <span class="price">Rs 3500</span>
               </li>
               <li>
                 <a href="#">
-                  <span class="product">Desi</span>
+                  <span class="product">Portrait of Joy</span>
                 </a>
-                <span class="price">Rs 800</span>
+                <span class="price">Rs 670</span>
+              </li>
+              <li>
+                <a href="#">
+                  <span class="product">House of the people</span>
+                </a>
+                <span class="price">Rs 2500</span>
+              </li>
+              <li>
+                <a href="#">
+                  <span class="product">Animal Life</span>
+                </a>
+                <span class="price">Rs 1500</span>
               <li>
               </ul>
             </div>
@@ -255,4 +289,56 @@
      </script>
      </body>
 
-     
+     <footer class="footer1">
+  
+        <div class="footer-left">
+        <h3>Ti<span>Moris</span></h3>
+        <p class="footer-links">
+        <a href="#">Home</a>
+        .
+        <a href="#">Login</a>
+        ·
+        <a href="#">About</a>
+        ·
+        <a href="#">Privacy Policy</a>
+        ·
+        <a href="#">Faq</a>
+        ·
+        <a href="#">Contact</a>
+        </p>
+        
+        <p class="footer-company-name">Ti Moris &copy; 2022</p>
+        </div>
+        
+        <div class="footer-center">
+        <div>
+        <i class="fa fa-map-marker"></i>
+        <p><span>Avenue des Palmiers </span> Curepipe,Mauritius</p>
+        </div>
+        
+        <div>
+        <i class="fa fa-phone"></i>
+        <p>+230 59484598</p>
+        </div>
+        
+        <div>
+        <i class="fa fa-envelope"></i>
+        <p><a href="SJ983@live.mdx.ac.uk">SJ983@live.mdx.ac.uk</a></p>
+        </div>
+        
+        </div>
+        
+        <div class="footer-right">
+        
+        <p class="footer-company-about">
+        <span>About the company</span>
+        Ecommerce website selling local paintings
+        </p>
+        <div class="footer-icons">
+        <a href="#"><i class="fa fa-facebook"></i></a>
+        <a href="#"><i class="fa fa-twitter"></i></a>
+        <a href="#"><i class="fa fa-linkedin"></i></a>
+        <a href="#"><i class="fa fa-github"></i></a>
+        
+        </div>  
+        </footer>
