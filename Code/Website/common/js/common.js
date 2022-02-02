a080e023c9586544ade6c04bbcfe993d56346dbc
Avinesh Culloo <avineshculloo@gmail.com>
2022-02-02 14:06:07 +0400

    create the toggle eye function

--- Code/Website/common/js/common.js
+++ Code/Website/common/js/common.js
@@ -8,6 +8,37 @@ const range = document.querySelector(".slider .progress");
 // price gap between min and max
 let priceGap = 500;
 
+//  variable to store the toggleEye details
+let toggle_name = document.getElementById("toggleEye");
+// variable to store the URL content
+let pathArray = window.location.pathname.split('/');
+// access the last element of pathname
+let pathname = pathArray[pathArray.length - 1];
+// check if pathname is setting.php or register.php
+
+if (pathname == 'register.php' ){
+    // event listener to detect click on the eye
+    toggle_name.addEventListener("click", togglePassword);
+}
+
+//  function for togglePaassword
+function togglePassword() {
+    // variables
+    let pwd = document.getElementById(toggle_name.getAttribute("name"));
+    let type = pwd.getAttribute("type");
+
+    // toggle the type attribute
+    if (type == 'password'){
+        type = 'text';
+    } else {
+        type = 'password';
+    }    
+    pwd.setAttribute('type', type);
+
+    //  change the icon 
+    this.classList.toggle('fa-eye-slash');
+}
+
 // list for price change
 priceInput.forEach(input =>{
     input.addEventListener("input", e =>{
