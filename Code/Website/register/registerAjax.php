571a9578bb7ceb92567378b35dd0457e817d4e2b
Avinesh Culloo <avineshculloo@gmail.com>
2022-02-03 13:54:04 +0400

    Create connection for register to Collection Customer

--- Code/Website/register/registerAjax.php
+++ Code/Website/register/registerAjax.php
@@ -1,4 +1,16 @@
 <?php
+    //Include libraries
+    require '../vendor/autoload.php';
+    
+    //Create instance of MongoDB client
+    $mongoClient = (new MongoDB\Client);
+
+    //Select a database
+    $db = $mongoClient->ecommerce;
+
+    //Select a collection 
+    $collection = $db->Customer;
+
     //Get strings - need to filter input to reduce chances of SQL injection etc.
     $firstname = filter_input(INPUT_POST, 'firstname', FILTER_SANITIZE_STRING);
     $lastname = filter_input(INPUT_POST, 'lastname', FILTER_SANITIZE_STRING); 
@@ -11,3 +23,25 @@
     $postalCode = filter_input(INPUT_POST, 'postalCode', FILTER_SANITIZE_NUMBER_INT);
     $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
     
+    //Convert to PHP array
+    $dataArray = [
+        "firstname" => $firstname,
+        "lastname" => $lastname,
+        "email" => $email, 
+        "DOB" => $DOB,
+        "gender" => $gender,
+        "telephone" => $telephone,
+        "address" => $address,
+        "city" => $city,
+        "postalCode" => $postalCode,
+        "password" => $password
+    ];
+
+    //Add the new user to the database
+    $insertResult = $collection->insertOne($dataArray);
+
+    // STORE REGISTRATION DATA IN MONGODB
+    //Echo result back to user
+    if($insertResult->getInsertedCount()!=1){
+        echo '<script>alert("Error adding customer")</script>';
+    }
