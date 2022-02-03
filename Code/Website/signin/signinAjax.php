48a9616ef9380fa49e399711bb4ed6ada54005ea
Avinesh Culloo <avineshculloo@gmail.com>
2022-02-03 13:55:31 +0400

    Add the composer file

--- Code/Website/signin/signinAjax.php
+++ Code/Website/signin/signinAjax.php
@@ -1,4 +1,16 @@
 <?php
+    //Include libraries
+    require '../vendor/autoload.php';
+    
+    //Create instance of MongoDB client
+    $mongoClient = (new MongoDB\Client);
+
+    //Select a database
+    $db = $mongoClient->ecommerce;
+
+    //Select a collection 
+    $collection = $db->Customer;
+    
     //Get email and password strings - need to filter input to reduce chances of SQL injection etc.
     $email= filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
     $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
