9a4e2a3a0ac80f77a1785046937f60e045f68008
Avinesh Culloo <avineshculloo@gmail.com>
2022-02-05 09:49:09 +0400

    New file: portrait.js

---
+++ Code/Website/portrait/js/portrait.js
@@ -0,0 +1,10 @@
+// Entire script will be in script mode
+"use strict";
+
+// function call 
+generateContent();
+
+// function to generate the content of the portrait body
+function generateContent(){
+    
+}
